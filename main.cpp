affcf62bbab5fb204ed53961c6b35d4176e8a56a
James Wobser <james.wobser@gmail.com>
2020-09-06 10:00:45 -0400

    Trying different things with boost

--- main.cpp
+++ main.cpp
@@ -7,36 +7,54 @@
 #include <fstream>
 #include <cassert>
 #include <unistd.h>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include <boost/regex.hpp>
+#include <boost/thread.hpp>
+#include <chrono>
 
-// Rcon Library
-#include "SourceRCON/include/srcon.h"
 
-// Discord Library - https://github.com/zeroxs/aegis.cpp
-// #include <aegis.hpp>
 
 // TOML Parsing Lib
 #include "tomlplusplus/toml.hpp"
 
-
 // TODO: Add timestamping for output lines
+// TODO: Open RCON connection
 
+// using boost::regex;
 using std::regex;
 using std::string;
 using std::string_view;
 using std::cout;
 using std::smatch;
+// using boost::smatch;
 using std::filesystem::exists;
 using std::filesystem::remove;
 using namespace std::literals;
-
+using namespace boost::asio::ip;
 // const int TIMEOUT {5};
-const std::filesystem::path confpath {"mcdown.yaml"};
+const std::filesystem::path confpath {"mcdown.toml"};
 const string cp = "mcdown.toml";
 const int MC_RCON_PORT = 25575;
 
+class rconpacket {
+	public:
+	rconpacket() :size{0}, ID{0}, type{0}, body{""} {};
+	rconpacket(int size, int ID, int type, string command);
+
+	private:
+	int32_t size = 0;
+	int32_t ID = 0;
+	int32_t type = 0;
+	string body;
+};
+
 int main(int argc, char *argv[]){
+	boost::asio::io_context io_context;
 	// Read RCON configuration file
 	auto config = toml::parse_file(cp);
+
+	// Get Values
 	string serveraddr = string(config["rcon"]["addr"].value_or(""sv));
 	string serverpw = string(config["rcon"]["passwd"].value_or(""sv));
 	const int serverport = config["rcon"]["port"].value_or(MC_RCON_PORT);
@@ -44,10 +62,32 @@ int main(int argc, char *argv[]){
 	cout << "Server addr: " << serveraddr << '\n';
 	cout << "Server empty flag: " << serverempty << "\n";
 
-	// TODO: Open RCON connection
-	// TODO: Get number of players
-	srcon mcrcon = srcon(serveraddr, serverport, serverpw);
-	string response = mcrcon.send("list");
+	tcp::socket mysock(io_context);
+	tcp::endpoint server = tcp::endpoint(address::from_string(serveraddr), 25575);
+	// mysock.connect(tcp::endpoint(server));
+	mysock.open(boost::asio::ip::tcp::v4());
+	cout << "Opened \n";
+	boost::asio::ip::tcp::socket::reuse_address option(true);
+	mysock.set_option(option);
+	cout << "Set Reuse address option \n";
+	mysock.bind(server);
+	cout << "Bound \n";
+	mysock.send(boost::asio::buffer("test"));
+	cout << "Sent \n";
+	// boost::asio::connect(mysock, server);
+	string command{"test"};
+	int packlen = 10 + command.length();
+	cout << "Message length: " << packlen << std::endl;
+
+	cout << "Waiting for 1s." << std::endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "Finished Waiting." << std::endl;
+	
+
+
+
+	// mysock.receive();
+	// mysock.open(boost::asio::ip::tcp::v4());
 
 	// cout << "Attempted to get RCON connection \n";
 	// string discard;
@@ -57,7 +97,7 @@ int main(int argc, char *argv[]){
 	/* Example Response:
 		There are 0 of a max of 20 players online:
 	*/
-	// string response { R"(There are 0 of a max of 20 players online:)" };
+	string response { R"(There are 0 of a max of 20 players online:)" };
 	cout << "Response: " << response << '\n';
 	int playercount = 0;
 	regex pat{ R"((?:There are )([0-9]+))" };
--- makefile
+++ makefile
@@ -1,13 +1,8 @@
+INC_PATH=/usr/local/include
+BOOST_ROOT=/usr/local/boost_1_73_0
 CXX = clang++
-CXXFLAGS = -Wall -g -std=c++17
+CXXFLAGS = -Wall -g -std=c++17 -I$(BOOST_ROOT) -pthread
 
-main: main.o ./SourceRCON/bin/rcon.o
-	$(CXX) $(CXXFLAGS) -o main main.o ./SourceRCON/bin/rcon.o
-main.o: main.cpp
-	$(CXX) $(CXXFLAGS) -c main.cpp
-
-SourceRCON/bin/rcon.o: SourceRCON/include/srcon.h SourceRCON/src/srcon.cpp
-	$(CXX) $(CXXFLAGS) -c ./SourceRCON/src/srcon.cpp -o ./SourceRCON/bin/rcon.o
-
-clean:
-	rm main.o main rcon.o
+main: main.cpp
+	$(CXX) $(CXXFLAGS) -o main main.cpp \
+		/usr/local/boost_1_73_0/stage/lib/libboost_thread.a
